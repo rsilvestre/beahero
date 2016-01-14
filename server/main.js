23aecad77294bcbc9462d578c12897469bf5be1e
Michael Silvestre <willtard@gmail.com>
2016-01-14 19:19:20 +0100

    Update package modify eslint to avoid to scan public files add compiler file

--- server/main.js
+++ server/main.js
@@ -18,7 +18,7 @@ app.use(convert(historyApiFallback({
 app.use(convert(function *(next) {
   var start = new Date()
   yield next
-  var ms = new Date - start
+  var ms = new Date() - start
   console.log('%s %s - %s', this.method, this.url, ms)
 }))
 
